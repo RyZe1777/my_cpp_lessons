d355447432b703c62ca7e8519b577bbc5dbacc9c
RyZe1777 <145759988+RyZe1777@users.noreply.github.com>
2023-10-13 09:12:31 +0000

    Create Codigos_1.cpp

---
+++ Codigos_1.cpp
@@ -0,0 +1,27 @@
+
+int main()
+{
+    cout << "Digita um numero: " ;   //imprime no ecra
+    cin >> numero;                   // le no teclado
+    cout << "O numero que digitaste foi: " << numero << endl;
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+// int main()
+// {
+//     cout <<  "Hello  World\n" ;
+//     cout << "I'm a C++ program\n";
+//     cout << O_MEU_NUMERO;
+//     cout << endl;
+//     return 0;
+// }
