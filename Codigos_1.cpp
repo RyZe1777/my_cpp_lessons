b221e3f6914fc3ac73af17e5762009401a9e4268
RyZe17 <miguelinho123pro@mgmail.com>
2023-10-13 14:18:13 +0000

    ola

--- Codigos_1.cpp
+++ Codigos_1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 
 int main()
 {
@@ -7,21 +9,180 @@ int main()
     return 0;
 
 }
+--------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+ int main() 
+ {
+
+ cout <<  "Hello  World\n" ;
+ cout << "I'm a C++ program\n";
+ cout << O_MEU_NUMERO;
+ cout << endl;
+ return 0;
+}
+--------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    string a = "1";
+    string b = "2";
+    cout << a+b << endl;     //impreme ecra
+    // cin >> numero;         //Le no teclado
+    // cout << "O numero que digitaste foi: "<< numero <<endl;
+    return 0;
+}
+---------------------------------------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+//area do circulo
+//é PI * raio * raio, calcular a area do circulo
+//com raio 10
+
+int main(){
+
+const double PI = 3.14159265359;
+int r = 10;
+double area = PI * r * r; 
+
+}
+------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = a+(b*c)+d; 
+    cout << resultado << endl;
+    return 0;
+
+}
+-----------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = a%c; 
+    cout << resultado << endl;
+    return 0;
+
+}
+------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = ++a; 
+    cout << resultado << endl;
+    return 0;
+
+}
+------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = --a; 
+    cout << resultado << endl;
+    return 0;
+
+}
+----------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
 
 
+int main()
+
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = a+=6; 
+    cout << resultado << endl;
+    return 0;
+
+}
+----------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
 
 
+int main()
+
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = a-=6; 
+    cout << resultado << endl;
+    return 0;
+
+}
+-----------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
 
 
+int main()
 
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = a-=2; 
+    cout << resultado << endl;
+    resultado = a+=2
+    cout << resultado << endl;
+    return 0;
 
+}
+------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
 
+int main()
 
-// int main()
-// {
-//     cout <<  "Hello  World\n" ;
-//     cout << "I'm a C++ program\n";
-//     cout << O_MEU_NUMERO;
-//     cout << endl;
-//     return 0;
-// }
+{
+    int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = a&=4; 
+    cout << resultado << endl;
+    return 0;
+}
+-----------------------------------------------------------------------------------------------
+
