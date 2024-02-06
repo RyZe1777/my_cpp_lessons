3b12d121f3f864302d229d522f9994d50250265f
RyZe1777 <miguelinho123pro@gmail.com>
2024-02-06 10:39:11 -0100

    aa

--- modulo2/fncprintf.cpp
+++ modulo2/fncprintf.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+
+
+string str; 
+int i;
 
 int main(){
-
-
-    
+    system("clear");
+    printf("Enter your Family name: ");
+    scanf ("%s", &str);
+    printf("Enter your age: ");
+    scanf("%d", &i);
+    printf("Mr. %s, %d years old. \n", &str, i);
+    return 0;
 }
+
