4740494b7f74235056564239074bbddfc05c90db
RyZe177 <antoniomariab7@gmail.com>
2024-09-23 10:39:01 +0000

    update

---
+++ 2 ANO/objects.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+Class Carro{
+    public:                              // especificador de acesso
+        //int velocidade;
+        //int velocidadeMax;
+        int anos;                       // atributo (int)
+        string marca;                   // atributo (string)
+        void setVelocidade(int v){      // meto
+            velocidade = v;
+        }
+        //void insere(string x, int y);
+        //void mostra();
+    private:
+        int velocidade;                 // atributo (int)
+}
+
+
+int main(){
+    Carro carro1;
+
+    carro1.anos = 15;
+    carro1.marca = "Porsche"
+
+    return 0;
+}
