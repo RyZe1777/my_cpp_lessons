d696c8ebefdfdbbadad21f0c6d62c77249c5a1c1
RyZe177 <antoniomariab7@gmail.com>
2024-10-14 17:30:26 +0000

    ExerciciosOOP

--- ExerciciosOOP/exercicio Fanta/exerciciofanta.cpp
+++ ExerciciosOOP/exercicio Fanta/exerciciofanta.cpp
@@ -1,5 +1,73 @@
 #include <iostream>
+#include <string>
 
-using namespace
+using namespace std;
 
-classe
+struct Refrigerante{
+    string marca;
+    string sabor;
+    string tipo;
+    string validade;
+    string ingredientes;
+    bool state; //falso-fechado vdd-aberto
+    bool agitada;
+        
+};
+
+
+class Refrigerante{
+
+    private:
+
+    bool state; //falso-fechado vdd-aberto
+    bool agitada;
+
+    public:
+
+        Refrigerante(){
+            this-> state = false;
+            this-> agitada = true;
+            cout << "A lata foi instanciada" <<endl;
+        }
+
+        void BeberLata(){
+            if(state){
+                cout << "Esta a bebendo o Refrigerante" <<endl;
+            }else{
+                cout << "Abra a lata Primeiro" <<endl;
+                AbrirLata();
+                cout << "Agora podes beber o Refrigerante" <<endl;
+            }
+        }
+
+        void AgitaLata(){
+            if(agitada){
+                cout << "Explodiu" <<endl;
+            }else{
+                cout << "Nao vai explodir" <<endl;
+            }
+        }
+
+        void AbrirLata(){
+            if(state){
+                cout << "A lata ja esta aberta" <<endl;
+                
+            }else{
+                cout << "Vamos Abrir a lata" <<endl;
+                this-> state = true;
+                AgitaLata();
+            }
+
+        }
+    
+};
+
+int main() {
+
+    Refrigerante latinha;
+    latinha.AbrirLata();
+    latinha.BeberLata();  // Try opening the can twice
+
+
+    return 0;
+}
